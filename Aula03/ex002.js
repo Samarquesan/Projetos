b88fca44c1be82a85ab955e3853c279d40352634
Samuel Marques dos Santos <78937543+Samarquesan@users.noreply.github.com>
2023-08-18 17:56:51 -0300

    atualização aula03

---
+++ Aula03/ex002.js
@@ -0,0 +1,8 @@
+function dividir(){
+    let result = n1 / n2
+    return result;
+}
+dividir(12, 3)
+let valor= result
+console.log(valor.value)
+
---
+++ Aula03/ex004.js
@@ -0,0 +1,2 @@
+let hora = new Date()
+        document.body.innerHTML= "<h1> Hora no exato momento:" + hora.getHours() + ":" + hora.getMinutes() + ":" + hora.getSeconds() + "</hora>"

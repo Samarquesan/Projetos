6d6523baedb9aa030ac1dc489eb2725056a1b21a
Samuel Marques dos Santos <78937543+Samarquesan@users.noreply.github.com>
2023-08-28 15:13:40 -0300

    Create ex001.php

---
+++ Aula04/ex001.php
@@ -0,0 +1,21 @@
+<?php
+    define("CONSTANT", "Hello world!");
+    echo CONSTANT; // Imprime "Hello world!"
+    echo Constant;  // Imprime "Constant" e gera um alerta notice.
+
+    define('ANIMALS', array(
+        'dog',
+        'cat',
+        'bird'
+    ));
+    echo ANIMALS[1] //imprime "cat"
+    //variaveis
+    $nome = "Samuel";
+    $idade = 32;
+    if($sexo == M){
+        $booleam = true
+    } else{
+        $booleam = false
+    }
+    $booleam = true
+?>
